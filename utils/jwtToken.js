746c74d98383ce109b4b4eae8a21de8ec875bcd7
Abhishek <Abhishekmishra@gmail.com>
2024-08-05 15:03:21 +0530

    token update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,19 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = async (user, message, statuscode, res) => {
-  try {
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "10h",
+  const token = user.generateJsonWebToken();
+  res
+    .status(statuscode)
+    .cookie("token", token, {
+      expires: new Date(Date.now() + 15 * 60 * 1000),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message,
+      user,
+      token,
     });
-    res
-      .status(statuscode)
-      .cookie("token", token, {
-        expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000), // Set expiration time for the cookie
-        httpOnly: true, // Cookie is not accessible via JavaScript (prevents XSS attacks)
-      })
-      .json({ success: true, message, user, token });
-  } catch (error) {
-    console.error("Error generating token:", error.message);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
-  }
 };
