dd3a214d8b4268e7c98ec9f600854ad315d55c06
Abhishek <Abhishekmishra@gmail.com>
2024-08-05 11:08:08 +0530

    jwtToken.js update removed sameSite: strict

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,7 +10,6 @@ export const generateToken = async (user, message, statuscode, res) => {
       .cookie("token", token, {
         expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000), // Set expiration time for the cookie
         httpOnly: true, // Cookie is not accessible via JavaScript (prevents XSS attacks)
-        sameSite: "strict", // Cookie is sent only for same-site requests
       })
       .json({ success: true, message, user, token });
   } catch (error) {
